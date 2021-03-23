c3331cb077a714a600cb739677a73a162a737c3b
Langston Barrett <langston.barrett@gmail.com>
2021-03-23 10:17:21 -0700

    nix/blight: Upgrade blight to 0.0.29

--- nix/blight.nix
+++ nix/blight.nix
@@ -2,18 +2,19 @@
 , buildPythonPackage
 , fetchPypi
 , click
+, pydantic
 , typing-extensions
 }:
 
 let pname = "blight";
-    version = "0.0.20";
+    version = "0.0.29";
 in buildPythonPackage rec {
   inherit pname version;
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0m0gng9iazwd3zivql76qlg6yr3269d1vcv48yjsxar4gai59aml";
+    sha256 = "0sg5z3m83nqhp7p0x1qmnzi3p48vynlrx0slz4pdfbbvawpw7s4w";
   };
   buildInputs = [ ];
-  propagatedBuildInputs = [ click typing-extensions ];
+  propagatedBuildInputs = [ click pydantic typing-extensions ];
   meta = {};
 }
