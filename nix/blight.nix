5cf8613fee0fc5add7beba85c5fb740bdac97d2a
Langston Barrett <langston.barrett@gmail.com>
2021-08-20 13:16:30 -0700

    Upgrade Blight to 0.0.33

--- nix/blight.nix
+++ nix/blight.nix
@@ -7,12 +7,12 @@
 }:
 
 let pname = "blight";
-    version = "0.0.29";
+    version = "0.0.33";
 in buildPythonPackage rec {
   inherit pname version;
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0sg5z3m83nqhp7p0x1qmnzi3p48vynlrx0slz4pdfbbvawpw7s4w";
+    sha256 = "1h52wwhqyml6zd5gckrq4cv83a4yjabvw5i60w5bbyibjsbh0d8s";
   };
   buildInputs = [ ];
   propagatedBuildInputs = [ click pydantic typing-extensions ];
